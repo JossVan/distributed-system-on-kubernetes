428ee034aba524adf6abd4e39daf43c8ffea10a6
jossvan <vanesitapolanco@gmail.com>
2021-12-24 00:21:31 -0600

    agregando redis al grpc

--- gRPC/grpcServer/go.mod
+++ gRPC/grpcServer/go.mod
@@ -3,6 +3,7 @@ module servergrpc
 go 1.13
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/golang/protobuf v1.4.3
 	go.mongodb.org/mongo-driver v1.5.2
 	google.golang.org/grpc v1.37.0
--- gRPC/grpcServer/server.go
+++ gRPC/grpcServer/server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	pb "servergrpc/proto-grpc"
 	"time"
 
+	"github.com/go-redis/redis"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -18,12 +20,65 @@ const (
 	port = ":50051"
 )
 
+// localhost = "127.0.0.1"
+// local redis container = "172.17.0.2"
+const ip_address = "34.125.139.194"
+
+var rdb = redis.NewClient(&redis.Options{
+	Addr:     ip_address + ":6379",
+	Password: "",
+	DB:       0, // use default DB
+})
+
+func setValues(person string) {
+	/* setear contador para rango de edades
+	RANGOS
+	rango1 -> ninos 0 - 11
+	rango2 -> adolescentes 12 - 18
+	rango3 -> jovenes 19 - 26
+	rango3 -> adultos 27 - 59
+	rango4 -> vejez >= 60
+	*/
+	persona := personas{}
+
+	if err := json.Unmarshal([]byte(person), &persona); err != nil {
+		log.Println("Error al convertir el struct a json")
+	}
+	var key = ""
+	if persona.Age >= 60 {
+		key = "rango5"
+	} else if persona.Age < 60 && persona.Age >= 27 {
+		key = "rango4"
+	} else if persona.Age <= 26 && persona.Age >= 19 {
+		key = "rango3"
+	} else if persona.Age <= 18 && persona.Age >= 12 {
+		key = "rango2"
+	} else {
+		key = "rango1"
+	}
+	_, err := rdb.Incr(key).Result()
+	if err != nil {
+		log.Println("Error al aumentar contador ", key)
+	}
+
+	/*	utilizar una lista
+		lpush lista <elemento>
+		y para obtener los ultimos 5
+		lrange lista 0 4
+	*/
+	data, _ := json.Marshal(person)
+	_, err = rdb.LPush("listaPersonas", data).Result()
+	if err != nil {
+		log.Println("Error al insertar informacion en la lista", err)
+	}
+}
+
 type personas struct {
-	name         string `json:"name"`
-	location     string `json:"location"`
-	age          int    `json:"age"`
-	vaccine_type string `json:"vaccine_type"`
-	n_dose       int    `json:"n_dose"`
+	Name         string `json:"name"`
+	Location     string `json:"location"`
+	Age          int    `json:"age"`
+	Vaccine_type string `json:"vaccine_type"`
+	N_dose       int    `json:"n_dose"`
 }
 
 type server struct {
@@ -63,6 +118,7 @@ func almacenar_persona(personas string) {
 
 func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInfo, error) {
 	almacenar_persona(in.GetId())
+	setValues(in.GetId())
 	fmt.Printf(">> Data recida: %v\n", in.GetId())
 	return &pb.ReplyInfo{Info: ">> persona: " + in.GetId()}, nil
 }
